9cebdbdf08a81be76bd411aa801b2881abe9beb8
theodasa <theophilly20@gmail.com>
2021-09-07 05:48:04 +0100

    added nav logo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 const body = document.body;
+var docWidth = document.documentElement.offsetWidth;
 const toggle = document.getElementById('nav-icon');
 
 toggle.addEventListener('click', () => {
   body.classList.toggle('active');
+});
+
+[].forEach.call(document.querySelectorAll('*'), function (el) {
+  if (el.offsetWidth > docWidth) {
+    console.log(el);
+  }
 });
